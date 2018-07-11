dd2b3e3bec1c570dc22144070539648e8209827a
William J Teskey <willjteskey@gmail.com>
2018-07-11 20:08:23 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,64 @@
+class Application
+
+  @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+    #resp.write handle_cart
+      if @@cart.empty? 
+        resp.write "Your cart is empty"
+       
+      else 
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end 
+      end 
+    elsif req.path.match(/add/)
+      add_term = req.params["item"]
+      resp.write handle_add(add_term)
+    else
+      resp.write "Path Not Found"
+    end
+
+    
+
+
+    resp.finish
+  end
+
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+  
+  def handle_add(add_term)
+    if @@items.include?(add_term)
+      @@cart << add_term
+      return "added #{add_term}"
+    else 
+      return "We don't have that item"
+    end 
+  end 
+  
+  def handle_cart
+    if @@cart != nil 
+      return @@cart.each {|item| "#{item}\n"}
+    else 
+      return "Your cart is empty"
+    end 
+  end
+end
